d5862a00b9181772ffe10667f8652b072911e820
AnushGurung11 <anushgrg15@gmai.com>
2025-06-20 11:53:21 +0545

    Students Class

---
+++ src/Class3/Students.java
@@ -0,0 +1,61 @@
+package Class3;
+/*
+a. id, name, grade(number)
+	b. Constructor (don't initialize value for grade)
+	c. Using the reference of Student object, only allow name to be directly changed (id, and grade should not be allowed to change directly)
+	d. Provide
+a method to change grade, and
+get grade (while getting grade don't return number, use condition to return A+, A, B+, B..... ) (encapsulation example)
+
+ */
+
+public class Students {
+    //instance variables
+    private final int id ;
+    public String name;
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGrade() {
+
+        if(this.grade<0){
+            return "Invalid";
+        }else{
+            if(this.grade>70 && this.grade <= 100){
+                return "A";
+            } else if(this.grade<70 && this.grade > 50){
+                return "B";
+            } else if(this.grade<50 && this.grade >40) {
+                return "C";
+            }else{
+                return "F";
+            }
+        }
+
+
+
+     }
+        public String getName() {
+        return name;
+    }
+
+    //method to change the grades of the student
+    public void changeGrade(int grade){
+        this.grade = grade;
+    }
+
+    public Students(int id , String name, int grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public static void main(String[] args){
+        Students student = new Students(1,"Anush",90);
+        System.out.println(student.getGrade());
+
+    }
+}
